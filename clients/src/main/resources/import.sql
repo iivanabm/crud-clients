49883ef0a45f77670cbeca8a0b7148c6fd7627f7
Ivana Marinho <ivanammarinho@hotmail.com>
2020-12-25 15:56:32 -0300

    Find client by id, exception handling

--- clients/src/main/resources/import.sql
+++ clients/src/main/resources/import.sql
@@ -1,2 +1,3 @@
 INSERT INTO tb_client (children, cpf, income, name) VALUES (3, '145789', 5900.99, 'Monica');
 INSERT INTO tb_client (children, cpf, income, name) VALUES (2, '897456', 3500, 'Ed');
+INSERT INTO tb_client (children, cpf, income, name) VALUES (1, '456879', 6800, 'Julia');
